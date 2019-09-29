38d48fb11974e09901be8fa78bd836eefe79fa24
Leonardo Guissi <leonardorguissi@gmail.com>
2019-09-29 17:54:35 -0300

    Added axios post method

--- src/Components/Mic.js
+++ src/Components/Mic.js
@@ -4,13 +4,19 @@ import MicrophoneOn                               from 'material-ui/svg-icons/av
 import MicrophoneOff                              from 'material-ui/svg-icons/av/stop';
 import PauseIcon                                  from 'material-ui/svg-icons/av/pause';
 import { ReactMic }                               from 'react-mic';
+import axios                                      from 'axios';
 
 require ('./styles.scss');
+
+var userStartStop = 0;
+const postURL = 'http://127.0.0.1:5000/hello';
+var interval;
 
 export default class Demo extends Component {
   constructor(props){
     super(props);
     this.state = {
+      classification: "Nothing yet...",
       blobObject: null,
       isRecording: false,
       isPaused: false
@@ -21,6 +27,13 @@ export default class Demo extends Component {
   }
 
   startOrPauseRecording= () => {
+    /*if(userStartStop) {
+      clearInterval(interval);
+      userStartStop = 0;
+    }
+    else {
+      userStartStop = 1;
+    }*/
     const { isPaused, isRecording } = this.state
 
     if(isPaused) {
@@ -30,6 +43,20 @@ export default class Demo extends Component {
     } else {
       this.setState({ isRecording: true })
     }
+    this.setState({classification: "Recording..."})
+
+    /*if(!userStartStop) {
+      return;
+    }
+
+    setTimeout(() => {
+      document.getElementById('stop').click();
+      document.getElementById('start-pause').click();
+    }, 4000);
+    interval = setInterval(() => {
+      document.getElementById('stop').click();
+      document.getElementById('start-pause').click();
+    },1000);*/
   }
 
   stopRecording= () => {
@@ -45,6 +72,25 @@ export default class Demo extends Component {
 
   onStop= (blobObject) => {
     this.setState({ blobURL : blobObject.blobURL });
+    let formData = new FormData();
+    console.log(blobObject.blob);
+    formData.append('file', blobObject.blob)
+    axios.post(
+      postURL,
+      formData,
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        }
+      }
+    )
+    .then((response) => {
+      this.setState({ classification: response.data});
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
   }
 
   onData(recordedBlob) {
@@ -72,16 +118,21 @@ export default class Demo extends Component {
             onSave={this.onSave}
             onData={this.onData}
             strokeColor="#ffffff" />
+          <div>
+            <audio ref="audioSource" controls="controls" src={blobURL}></audio>
+          </div>
           <br />
           <br />
           <FloatingActionButton
             className="btn"
+            id="start-pause"
             secondary={true}
             onClick={this.startOrPauseRecording}>
             { (isRecording && !isPaused )? <PauseIcon /> : <MicrophoneOn /> }
           </FloatingActionButton>
           <FloatingActionButton
             className="btn"
+            id="stop"
             secondary={true}
             disabled={!isRecording}
             onClick={this.stopRecording}>
@@ -93,14 +144,14 @@ export default class Demo extends Component {
             Result:
           </h2>
           <h3 class="classification-title">
-            Nothing yet...
+            {this.state.classification}
           </h3>
           <br/>
           <br/>
           <br/>
           <br/>
           <div class="fav-icon-source">
-            Listen icon by <a target="_blank" href="https://icon-icons.com">Icons-Icons</a>
+            Listen icon by <a href="https://icon-icons.com">Icons-Icons</a>
           </div>
 
         </div>
